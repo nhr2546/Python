7c70e1557af78dbc03cd52adda0d70144c202fad
Nhr2546 <nhr2546@aol.com>
2018-09-13 23:57:57 +0100

    Load to new repository

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -7,3 +7,4 @@ class TestCases(base_test.BaseTest):
         assert self.loginpage.login(self.mainpage, "testuser", "testing")
         # Step 3: Logout of the webpage and confirm it returs to the welcome page
         assert self.mainpage.click_logout(self.welcomepage)
+        # Last line
